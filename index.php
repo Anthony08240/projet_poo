c17e7975a80d4aae635e8b1296af50f714148add
Anthony08240 <beenen@simplon-charleville.fr>
2020-04-14 13:54:43 +0200

    copié colé pour comencé

---
+++ index.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jeux poo</title>
+</head>
+<body>
+    <p>coucou tout le monde</p>
+</body>
+</html>
